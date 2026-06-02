76ce285efa875fd6d29dfb15693377282975f5c1
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-06-02 10:31:12 +0200

    Remove env file before we start any handling of keys

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
-    app_name: str = "App"
+    app_name: str = "Default Name"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 config = Config()
