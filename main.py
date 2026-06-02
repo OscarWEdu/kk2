3865ade48023634335a3564bc4223e8e7cf631d2
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-06-02 09:21:23 +0200

    Set up fastapi framework

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from kk2!")
+#Run with "uv run fastapi dev main.py" 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#Currying function
+def log_calls(func):
+    async def wrapper(*args, **kwargs):
+        print(f"calling {func.__name__}")
+        result = await func(*args, **kwargs)
+        print(f"done {func.__name__}")
+        return result
+    return wrapper
 
 
-if __name__ == "__main__":
-    main()
+@log_calls
+@app.get("/")
+async def read_root():
+    return {"hello": "world"}
+    
+@log_calls
+@app.get("/items/{item_id}")
+async def read_items(item_id: int, q: str | None = None):
+    return {"items_id": item_id, "q": q}
