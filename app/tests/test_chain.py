050033dcefc1d3183a47a661429c9db426b0c5b3
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-06-03 23:36:33 +0200

    Add test for SmolLM runnable

--- app/tests/test_chain.py
+++ app/tests/test_chain.py
@@ -14,3 +14,23 @@ def test_runnable_history():
     out = history.invoke({"question": "some string"})
     assert "User: q1" in out["context"]
     assert "LLM: a1" in out["context"]
+
+
+def test_runnable_smollm(monkeypatch):
+    test_answer = "42."
+
+    original_init = SmolLM.__init__
+    # call Pydantic's init but mock the SmolLM init
+    def mock_init(self):
+        original_init(self)
+        self.pipe = None
+
+    def mock_invoke(self, input):
+        return {"answer": test_answer, **input}
+
+    monkeypatch.setattr(SmolLM, "__init__", mock_init)
+    monkeypatch.setattr(SmolLM, "invoke", mock_invoke)
+    llm = SmolLM()
+    result = llm.invoke({"prompt": "What is the meaning of testing?", "context": ""})
+
+    assert result["answer"] == test_answer
