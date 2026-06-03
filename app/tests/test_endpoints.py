c8cd62e9dc1681765bcbe83b647061f3c9609de2
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-06-03 20:59:49 +0200

    Some more simple tests

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -6,3 +6,11 @@ client = TestClient(app)
 def test_endpoint_root():
     response = client.get("/")
     assert response.status_code == 200
+
+def test_endpoint_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_endpoint_data_stats_no_data():
+    response = client.get("/data/stats")
+    assert response.status_code == 404
